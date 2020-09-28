4db91fa0dfad82c95db02b745a240c87ba26b363
Noble Mungu <mungunoble@gmail.com>
2020-09-28 08:03:04 +0300

    add navabar

--- config.py
+++ config.py
@@ -3,3 +3,12 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     SECRET_KEY=os.environ.get('SECRET_KEY')
+class ProdConfig(Config):
+    DEBUG = True
+
+class DevConfig(Config):
+    """
+    DevConfig
+
+    """
+
