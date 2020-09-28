f8e480f71e244419b2f6a12fde0cd9418726a766
Noble Mungu <mungunoble@gmail.com>
2020-09-28 12:12:29 +0300

    add main route

--- app/main/views.py
+++ app/main/views.py
@@ -1,1 +1,29 @@
-views.py
+from flask import render_template,redirect,url_for,abort,request,flash
+from app.main import main
+from app.models import User,Blog,Comment
+from .forms import UpdateProfile,CreateBlog
+from .. import db
+from app.requests import get_quotes
+from flask_login import login_required,current_user
+from ..email import mail_message
+import secrets
+import os
+# from PIL import Image
+
+@main.route('/')
+def index():
+    quotes = get_quotes()
+    page = request.args.get('page',1, type = int )
+    blogs = Blog.query.order_by(Blog.posted.desc()).paginate(page = page, per_page = 3)
+    return render_template('index.html', quote = quotes,blogs=blogs)
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_filename = random_hex + f_ext
+    picture_path = os.path.join('app/static/photos', picture_filename)
+    
+    output_size = (200, 200)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_filename
